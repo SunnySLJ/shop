9a7dce1ecd76de010dd97206ca2a5d5945e1e423
lishuang <sunny.li@e-ai.com.cn>
2019-02-19 15:44:22 +0800

    no message

---
+++ sunny-zipkin/src/main/java/com/sunny/SunnyZipkinApplication.java
@@ -0,0 +1,17 @@
+package com.sunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableZipkinServer
+public class SunnyZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunnyZipkinApplication.class, args);
+	}
+}
